cb8e289f417ad2e5d83a52c5936bc6110f944835
MasudDeveloper <pkmasud1@gmail.com>
2025-01-16 12:08:55 +0600

    R.B Tours Version 0.0.1

---
+++ app/src/main/java/com/mrdeveloper/webviewpractice/MainActivity.java
@@ -0,0 +1,153 @@
+package com.mrdeveloper.webviewpractice;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    LottieAnimationView lottieAnimationView;
+
+    SwipeRefreshLayout refreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.refreshLayout), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        webView = findViewById(R.id.webView);
+        lottieAnimationView = findViewById(R.id.animationView);
+        refreshLayout = findViewById(R.id.refreshLayout);
+
+        refreshLayout.setColorSchemeColors(Color.RED,Color.GREEN,Color.YELLOW,Color.BLUE);
+
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+
+                String url = request.getUrl().toString();
+
+                if (url.startsWith("tel:")){
+                    Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                } else if (url.startsWith("mailto:")) {
+                    Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse(url));
+                    startActivity(intent);
+                    return true;
+                } else if (url.startsWith("https://www.facebook.com") || url.startsWith("fb://")) {
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true; // WebView এ লোড করা বন্ধ করা
+                } else if (url.startsWith("https://drive.google.com")){
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                    startActivity(intent);
+                    return true; // WebView এ লোড করা বন্ধ করা
+                }
+
+                return false;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                refreshLayout.setRefreshing(false);
+            }
+        });
+
+        webView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+                if (scrollY == 0) {
+                    refreshLayout.setEnabled(true);
+                } else {
+                    refreshLayout.setEnabled(false);
+                }
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+
+                if (newProgress == 100) {
+                    lottieAnimationView.setVisibility(View.GONE);
+                } else {
+                    lottieAnimationView.setVisibility(View.VISIBLE);
+                }
+            }
+        } );
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+
+
+
+        webView.loadUrl("https://rbtbd.com/");
+
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                webView.reload();
+            }
+        });
+
+
+
+
+        getOnBackPressedDispatcher().addCallback(new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                if (webView.canGoBack()){
+                    webView.goBack();
+                } else {
+                    new AlertDialog.Builder(MainActivity.this)
+                            .setTitle("Exit")
+                            .setMessage("Are You Sure Exit")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    finishAndRemoveTask();
+                                }
+                            }).setNegativeButton("No",null)
+                            .show();
+                }
+            }
+        });
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/mrdeveloper/webviewpractice/SplashScreenActivity.java
@@ -0,0 +1,52 @@
+package com.mrdeveloper.webviewpractice;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation animationTV, animationIV;
+
+    ImageView imageView;
+    TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+
+        animationTV = AnimationUtils.loadAnimation(SplashScreenActivity.this, R.anim.slide_in_left);
+
+        imageView.setAnimation(AnimationUtils.loadAnimation(this, R.anim.zoom_in));
+        textView.setAnimation(animationTV);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreenActivity.this,MainActivity.class));
+                finish();
+            }
+        },2000);
+
+    }
+}
